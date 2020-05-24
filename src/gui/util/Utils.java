e4569166ae0e3d77b964fcd1073af7f9f0c7ea4d
Natalia Tavares <natalia.h.tavares@gmail.com>
2020-05-24 17:58:58 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String str) { // método para converter Integer para String
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) { // formatando modelo
 																									// de Data
